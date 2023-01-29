4a5508b586fac6426a1e56e1dc42b6cc3b1a2900
nedia <aiden@nedia.dev>
2023-01-29 15:26:12 +0000

    Latest 2023-01-29 15:26:12.791874357 +00:00

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("config.plugins", {
+require("lazy").setup("plugins", {
   defaults = { lazy = true },
   dev = { path = "~/Code" },
   -- concurrency = 1,
