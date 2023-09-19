25ab4e161053060e285fd50f7339e60af73bfb26
nedia <aiden@nedia.dev>
2023-09-19 16:41:48 +1200

    Latest 2023-09-19 16:41:48.460699982 +12:00

--- lua/core/plugins/code.lua
+++ lua/core/plugins/code.lua
@@ -1,44 +1,39 @@
 return {
   {
-    'jose-elias-alvarez/null-ls.nvim',
-    enabled = false,
-    lazy = true,
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = function()
-      local nls = require('null-ls')
-      return {
-        sources = {
-          nls.builtins.diagnostics.fish,
-          nls.builtins.diagnostics.flake8,
-          nls.builtins.diagnostics.selene,
-          nls.builtins.diagnostics.shellcheck,
-          nls.builtins.formatting.black,
-          nls.builtins.formatting.fish_indent,
-          nls.builtins.formatting.isort,
-          nls.builtins.formatting.prettier,
-          nls.builtins.formatting.shfmt,
-          nls.builtins.formatting.stylua,
-        },
-      }
-    end,
-  },
-  {
     'stevearc/conform.nvim',
     lazy = true,
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
       formatters_by_ft = {
-        bash = { 'shellcheck', 'shfmt' },
+        bash = { 'shfmt' },
         fish = { 'fish_indent' },
         javascript = { 'prettier' },
         lua = { 'stylua' },
         python = { 'isort', 'black' },
-        sh = { 'shellcheck', 'shfmt' },
+        sh = { 'shfmt' },
       },
       format_on_save = {
         lsp_fallback = true,
         timeout_ms = 500,
       },
     },
+  },
+  {
+    'mfussenegger/nvim-lint',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function(_, _)
+      require('lint').linters_by_ft = {
+        bash = { 'shellcheck' },
+        lua = { 'selene' },
+        python = { 'flake8' },
+        sh = { 'shellcheck' },
+      }
+      vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
   },
 }
